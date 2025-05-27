9b7f22dbfcb4817929389fd899635b967935ba94
TheDanvag <damianvaldovinos22@aragon.unam.mx>
2025-05-27 13:59:16 -0600

    Create Rol.java

---
+++ Rol.java
@@ -0,0 +1,18 @@
+package com.govdatos.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre;
+}
